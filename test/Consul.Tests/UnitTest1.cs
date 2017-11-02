7a645090d7ba5593e4b8e77ada3d03916b4ab503
joao.reis <joao.reis@farfetch.com>
2017-11-02 19:48:13 +0000

    added unit tests project

---
+++ test/Consul.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Consul.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
